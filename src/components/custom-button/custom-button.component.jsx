93fcfa0d4b8276df6ac3550c481b1d715c7caa33
xander080 <irvinbahan@gmail.com>
2021-11-21 16:08:28 +0800

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -9,9 +9,7 @@ const CustomButton = ({
   ...otherProps
 }) => (
   <button
-    className={`${inverted ? 'inverted' : ''} ${
-      isGoogleSignin ? 'google-sign-in' : ''
-    } custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${isGoogleSignin} custom-button`}
     {...otherProps}
   >
     {children}
